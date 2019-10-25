08f8ef65771bfabedae42a7ff31cea3656b381ba
changpzh <changping.zhou@foxmail.com>
2019-10-25 17:55:48 +0800

    navLink demo

--- src/index.js
+++ src/index.js
@@ -1,60 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Route, NavLink} from 'react-router-dom';
 import './index.css';
 const { render } = ReactDOM;
 
 
-/* replace 为true的时，点击链接后将使用新地址替换掉上一次访问的地址*/
+const isActiveFunc = (match) => {
+    console.log(match,'contact');
+    return match;
+};
+
 const Links = () => (
     <nav>
-        <Link to="/">主页</Link>
-        <Link to="/about">About-页面</Link>
-        <Link replace to="/contact">Contact-页面</Link>
-        <Link to="/user">User</Link>
-        <Link to="/nested">Nested-页面</Link>
+        <NavLink exact activeClassName="active" to="/">Home</NavLink>
+        <NavLink activeStyle={{color: 'green', fontWeight: 'bold'}} to={{pathname: '/about'}}>About</NavLink>
+        <NavLink
+            isActive={isActiveFunc}
+            activeClassName="active"
+            to="/contact">Contact</NavLink>
     </nav>
-);
+)
 
 const App = () => (
-    <BrowserRouter>
+    <BrowserRouter basename={'/zhou'}>
         <div>
             <AddressBar/>
+
             <Links />
+
             <Route exact path="/" render={() => <h1>Home</h1>} />
             <Route path="/about" render={() => <h1>About</h1>} />
             <Route path="/contact" render={() => <h1>Contact</h1>} />
-            <Route path="/user" render={UserNested} />
-            <Route path="/nested" render={Nested} />
         </div>
     </BrowserRouter>
-);
-const UserNested = () => (
-    <div>
-        <Link to={"/user/zhou"}>zhou</Link>
-        <Link to={"/user/chang"}>chang</Link>
-        <Link to={"/user/ping"}>ping</Link>
-        <Route path={"/user/:username"} render={({match}) => <h2>URL: {(match.params.username || "opos!").toUpperCase()}</h2> }/>
-    </div>
-);
-
-
-const Nested = () => (
-    <div>
-        <Link to="/nested/one">One</Link>
-        <Link to="/nested/two">Two</Link>
-        <Link replace to="/nested/Three">Three</Link>
-        <div>选择一个点击</div>
-        <Route path="/nested/:minooo?" render={({match}) => <h2>URL: {match.params.minooo || 'minooo'}</h2>} />
-    </div>
-);
-
-
-
-const User = ({ match }) => {
-    console.log("zhoucpaing math:=%j", match);
-    return <h1>Hello {match.params.username}!</h1>
-};
+)
 
 /* 为了展示URL的变化的组件 请无视我*/
 const AddressBar = () => (
@@ -74,8 +53,8 @@ const AddressBar = () => (
             </div>
             <div className="url">URL: {location.pathname}</div>
         </div>
-
     )}/>
-);
+)
+
 
 render(<App/>, document.getElementById('root'))
